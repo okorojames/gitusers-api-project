c575e8b132ba8c84996238f254f346c9bf16896a
okorojames <okorojameschizaram@gmail.com>
2023-01-12 17:22:52 +0100

    ci/cd

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 const body = document.querySelector('body')
 const lightMode = document.querySelector('.light--mode')
 const darkMode = document.querySelector('.dark--mode')
+const appWrap = document.querySelector('.app-wrap')
 // LIGHT MODE
 lightMode.addEventListener('click', function () {
   lightMode.style.display = 'none';
   darkMode.style.display = 'block';
-  body.style.background = '#edf2ff'
+  body.style.background = '#edf2ff';
+  appWrap.classList.add('darkIt')
 })
 // DARK MODE
 darkMode.addEventListener('click', function () {
   lightMode.style.display = 'block';
   darkMode.style.display = 'none';
   body.style.background = '#050814'
+  appWrap.classList.remove('darkIt')
 })
